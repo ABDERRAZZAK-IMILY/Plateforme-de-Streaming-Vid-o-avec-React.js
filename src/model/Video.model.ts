2f4d580751fe2b3dbdf4ef07304eb7ce1c8c2af8
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-02-04 09:46:30 +0100

    feat: define Video interface with essential properties

---
+++ src/model/Video.model.ts
@@ -0,0 +1,14 @@
+export interface Video {
+    id: string;
+    title: string;
+    description: string;
+    thumbnailUrl: string;
+    trailerUrl: string;
+    duration: string;
+    releaseYear: number;
+    type: 'FILM' | 'SERIE' | 'DOCUMENTAIRE';
+    category: string;
+    rating: number;
+    director: string;
+    cast: string[];
+}

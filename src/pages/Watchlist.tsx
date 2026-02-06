afb3816e1927db66e6d76243656e61e2d1bc3215
IMILY ABDERRAZZK <imily2024@gmail.com>
2026-02-06 14:51:45 +0100

    fix config vite

--- src/pages/Watchlist.tsx
+++ src/pages/Watchlist.tsx
@@ -1,5 +1,12 @@
 export default function Watchlist() {
     return (
-        <div></div>
+        <>
+            <div className="join">
+                <button className="join-item btn btn-md">1</button>
+                <button className="join-item btn btn-md btn-active">2</button>
+                <button className="join-item btn btn-md">3</button>
+                <button className="join-item btn btn-md">4</button>
+            </div>
+        </>
     );
 }
